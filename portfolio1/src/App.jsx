aedddfba475ac1e34e390c5b4369853e23a4a7f6
Sudarsanan8 <psudarsanan27@gmail.com>
2024-06-14 13:38:18 +0530

    commited

---
+++ portfolio1/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-800 font-signature decoration-0">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
